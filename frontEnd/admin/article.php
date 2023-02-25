2f5d835284ae6d7f6914553bd77414d8117d0350
Vu Trong Dat <datvuhp2002@gmail.com>
2023-02-25 14:18:59 +0700

    update: cơ cấu lại code trong các file

---
+++ frontEnd/admin/article.php
@@ -0,0 +1,8 @@
+<?php
+    declare(strict_types = 1);                                          // Use strict types
+    require '../../backEnd/includes/database-connection.php';              // Create PDO object
+    require '../../backEnd/includes/functions.php';                        // Include functions
+?>
+<?php include "../includes/headerAdmin.php";?>
+<main></main>
+<?php include "../includes/footerAdmin.php";?>

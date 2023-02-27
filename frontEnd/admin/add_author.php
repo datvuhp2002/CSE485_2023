aacc1193560fe519e9d4c8a3972c15c78acaffbc
MinhDuckk <minhducnguyen257@gmail.com>
2023-02-27 14:53:15 +0700

    update: thêm add author và edit author

---
+++ frontEnd/admin/add_author.php
@@ -0,0 +1,25 @@
+<?php
+    declare(strict_types = 1);                                          // Use strict types
+    require '../../backEnd/includes/database-connection.php';              // Create PDO object
+    require '../../backEnd/includes/functions.php';                        // Include functions
+?>
+<?php include "../includes/headerAdmin.php";?>
+    <main class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-sm">
+                <h3 class="text-center text-uppercase fw-bold">Thêm mới tác giả</h3>
+                <form action="process_add_category.php" method="post">
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tên tác giả</span>
+                        <input type="text" class="form-control" name="txtCatName" >
+                    </div>
+
+                    <div class="form-group  float-end ">
+                        <input type="submit" value="Thêm" class="btn btn-success">
+                        <a href="author.php" class="btn btn-warning ">Quay lại</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+<?php include "../includes/footerAdmin.php";?>
--- frontEnd/admin/author.php
+++ frontEnd/admin/author.php
@@ -2,7 +2,39 @@
     declare(strict_types = 1);                                          // Use strict types
     require '../../backEnd/includes/database-connection.php';              // Create PDO object
     require '../../backEnd/includes/functions.php';                        // Include functions
+    $sql = "SELECT * FROM `tacgia` ORDER by tacgia.ma_tgia";
+    $author = pdo($pdo,$sql)->fetchAll();
 ?>
 <?php include "../includes/headerAdmin.php";?>
-<main></main>
+<main class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-sm">
+                <a href="add_author.php" class="btn btn-success">Thêm mới</a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Tên tác giả</th>
+                            <th >Sửa</th>
+                            <th >Xóa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach($author as $author) {?>
+                            <tr >
+                                <th name = "id" scope="row"><?= html_escape($author['ma_tgia'])?></th>
+                                <td ><?= html_escape($author['ten_tgia']) ?></td>
+                                <td>
+                                    <a href=""><i class="fa-solid fa-pen-to-square"></i></a>
+                                </td>
+                                <td >
+                                    <a href=""><i class="fa-solid fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
 <?php include "../includes/footerAdmin.php";?>

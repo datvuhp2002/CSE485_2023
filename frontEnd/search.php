d5373db3ee3ea18efe64273c1e562df2ffd54b34
Vu Trong Dat <datvuhp2002@gmail.com>
2023-03-02 13:33:57 +0700

    update: search

--- frontEnd/search.php
+++ frontEnd/search.php
@@ -2,18 +2,38 @@
     declare(strict_types = 1);                                          // Use strict types
     require '../backEnd/includes/database-connection.php';              // Create PDO object
     require '../backEnd/includes/functions.php'; 
-
-    if(isset($_GET['search'])){
-        $search = $_GET('search');
-        $sql = "SELECT * FROM baiviet WHERE ten_bhat LIKE '%search%'";
-        $result = $pdo->query($sql);
-
-        if($result->rowCount()>0){
-           $row = $result->fetch(PDO::FETCH_ASSOC);
-           echo "<script>alert('Tìm thấy bài viết'); window.location.href='detail.php?ten_bhat=" . urlencode($post['ten_bhat']) . "';</script>";
-        } else{
-
-            echo "<script>alert('Không có bài viết tương ứng')</script>";
-        }
+    $sql = "SELECT * FROM baiviet WHERE ten_bhat LIKE :search";
+    $result = $pdo->prepare($sql);
+?>
+<?php
+    session_start();
+    if( !isset($_SESSION['username'])){
+        include "includes/header.php";
+    }else{
+        include "includes/headerIsLogin.php";
     }
 ?>
+<main>
+    <?php 
+        $result->execute(['search' => '%'.$_GET['search'].'%']);
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){ 
+            $ma_bviet = $row['ma_bviet'];
+            ?>
+                <a href="detail.php?id=<?= $row['ma_bviet'] ?>" class="text-decoration-none col-md-3 col-sm-6 text-black">
+                    <div class="card card-block">
+                        <div class = "p-3">
+                        <img src="<?=func_get_img($row['hinhanh']) ?>" class="card-img-top">
+                            <h4 class="card-title mt-3 mb-3 fw-bold">
+                                    <?= html_escape($row['ten_bhat']) ?>
+                            </h4>
+                            <p class="card-text"><?= html_escape($row['tomtat']) ?>p> 
+                        </div>
+                    </div>
+                </a>
+            <?php 
+        }
+    ?>
+</main>
+
+
+<?php include "includes/footer.php";?>
